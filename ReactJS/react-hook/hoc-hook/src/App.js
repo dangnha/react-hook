656b0c96f696d78cb03ea52ce21b5b3f4f22a0b6
dangnha <nhand.21it@vku.udn.vn>
2023-04-17 05:52:02 +0700

    #4

--- ReactJS/react-hook/hoc-hook/src/App.js
+++ ReactJS/react-hook/hoc-hook/src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
+const App = () => {
+  let name = "Dang Nha";
+  let status = " is learning react hook";
+  let link =
+    "https://www.youtube.com/watch?v=Y9gTouaZJ5s&list=PLncHg6Kn2JT4xzJyhXfmJ53dzwVbq-S_E&index=11";
+  let obj = { name: "kufu", status: " is learning print obj" };
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello world!, {name} + {status}
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href={link}
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          This current video
         </a>
+        <p>{JSON.stringify(obj)}</p>
       </header>
     </div>
   );
-}
+};
 
 export default App;
