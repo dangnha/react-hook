4b8d91d4b7dea47282bc520eb7fb7ad6bc64bc7e
dangnha <nhand.21it@vku.udn.vn>
2023-04-20 07:12:41 +0700

    16.1.1

--- ReactJS/react-hook/hoc-hook/src/App.js
+++ ReactJS/react-hook/hoc-hook/src/App.js
@@ -4,24 +4,15 @@ import Nav from "./views/Nav.js";
 import "./Style/global.scss";
 import { useState } from "react";
 import TodoApp from "./views/TodoApp.js";
+import Covid19 from "./views/Covid19.js";
 
 const App = () => {
-  let link =
-    "https://www.youtube.com/watch?v=lTFPU8mA8kI&list=PLncHg6Kn2JT4xzJyhXfmJ53dzwVbq-S_E&index=22";
-
   return (
     <div className="App">
       <Nav></Nav>
       <header className="App-header">
-        <TodoApp></TodoApp>
-        <a
-          className="App-link"
-          href={link}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Link of this video
-        </a>
+        {/* <TodoApp></TodoApp> */}
+        <Covid19></Covid19>
       </header>
     </div>
   );
